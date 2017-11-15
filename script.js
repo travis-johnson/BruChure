0d59638cd8228e2e385e30ec17dd203b136cda1f
travis-johnson <trav.johnson919@gmail.com>
2017-11-15 15:49:51 -0500

    results have been styled better but sorting is not yet working.

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ app.controller('dataCtrl', function($scope, $http) {
     $http.get(proxy + "https://s3.amazonaws.com/bruvue-data/beer-data.json")
         .then(function(response) {
             $scope.beerInfo = response.data;
-            for (var i = 0; i <= response.data.length; i++) {
-                console.log(response.data[i]['Brewery Name']);
+            // for (var i = 0; i <= response.data.length; i++) {
+            //     console.log(response.data[i]['Brewery Name']);
 
-            }
-
+            // }
 
         });
 });
