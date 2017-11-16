292403c86da2ab109ba6ea145fd4a9a7f938d4f0
Travis Johnson <trav.johnson919@gmail.com>
2017-11-16 17:40:15 -0500

    results are able to be filtered via the input in the navbar

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var app = angular.module('bruVue', []);
 var proxy = "https://cors-anywhere.herokuapp.com/";
-app.controller('dataCtrl', function($scope, $http) {
+app.controller('dataCtrl', function($scope, $filter, $http) {
     $http.get(proxy + "https://s3.amazonaws.com/bruvue-data/beer-data.json")
         .then(function(response) {
             $scope.beerInfo = response.data;
+            // $scope.predicate = 'Beer Name';
+            $scope.sortType = 'Beer Name'; // set the default sort type
+            $scope.sortReverse = false; // set the default sort order
+            $scope.searchBeer = ''; // set the default search filter term
+            // console.log($scope.predicate);
             // for (var i = 0; i <= response.data.length; i++) {
-            //     console.log(response.data[i]['Brewery Name']);
+            //     // console.log(response.data[i]['Beer Name']);
+            //     $scope.predicate = response.data[i];
+
 
             // }
-
         });
 });
