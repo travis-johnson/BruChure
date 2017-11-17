3226ccbc879b07f54ee34d46b015c6dda7e237a4
Travis Johnson <trav.johnson919@gmail.com>
2017-11-17 15:01:58 -0500

    Sorting can now be done with the beer names.  Moving onto coding the rest of the columns to be sorted.

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 var app = angular.module('bruVue', []);
 var proxy = "https://cors-anywhere.herokuapp.com/";
 app.controller('dataCtrl', function($scope, $filter, $http) {
+    var sourceBeerData;
+    $scope.sortBy = sortBeerData;
     $http.get(proxy + "https://s3.amazonaws.com/bruvue-data/beer-data.json")
         .then(function(response) {
-            $scope.beerInfo = response.data;
+            sourceBeerData = response.data;
+            sourceBeerData.forEach(function(beer) {
+                for (var key in beer) {
+                    var newKey = key.replace(' ', '_');
+                    beer[newKey] = beer[key];
+                }
+                return beer;
+            });
+
+            sortBeerData();
+
             // $scope.predicate = 'Beer Name';
-            $scope.propertyName = 'Beer%Name'; // set the default sort type
+            $scope.propertyName = 'Beer_Name'; // set the default sort type
             $scope.reverse = true; // set the default sort order
             $scope.searchBeer = ''; // set the default search filter term
-
-            $scope.sortBy = function(propertyName) {
-                $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-                $scope.propertyName = propertyName;
-            };
         });
+
+    function sortBeerData() {
+        $scope.beerInfo = sourceBeerData.sort(beerSort);
+    }
+
+    function beerSort(a, b) {
+        var output = 0;
+        if (a[$scope.propertyName] < b[$scope.propertyName]) output = -1;
+        if (a[$scope.propertyName] > b[$scope.propertyName]) output = 1;
+        if ($scope.reverse) output = -output;
+        return output;
+    };
 });
